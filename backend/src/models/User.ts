a46b9523f8aeed248c3dfebf77a6ff198cb59772
Antonio Barros <antonio.barros.alcantara.neto@ccc.ufcg.edu.br>
2025-11-26 11:09:54 -0300

    feat: Adiciona modelo da entidade usuário

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -2,24 +2,59 @@ import {Model, DataTypes, Optional} from 'sequelize';
 import sequelize from '../config/database';
 
 export interface UserAttributes {
+    id: number;
     cpf: string;
     phoneNumber: string;
+    name: string;
     email: string;
     password: string;
 }
 
 export interface UserCreationAttributes
-    extends Optional<UserAttributes, "cpf"> {}
+    extends Optional<UserAttributes, "id"> {}
 
 export class User
     extends Model<UserAttributes, UserCreationAttributes> 
     implements UserAttributes 
 {
+    public id!: number;
+    public name!: string;
     public cpf!: string;
     public phoneNumber!: string;
     public email!: string;
     public password!: string;
 }
 
-
-
+User.init(
+    {
+        id:{
+            type:DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type:DataTypes.STRING,
+            allowNull: false
+        },
+        cpf: {
+            type:DataTypes.STRING,
+            allowNull: false
+        },
+        phoneNumber: {
+            type:DataTypes.STRING,
+        },
+        email: {
+            type:DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type:DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        tableName: "users",
+        timestamps: false
+    }
+)
--- backend/src/repository/UserRepository.ts
+++ backend/src/repository/UserRepository.ts
@@ -1,1 +1,19 @@
 import { User } from "../models/User";
+
+export class UserRepository {
+    async createUser(name: string, cpf: string, phoneNumber:string, email: string, password:string) {
+        const user = await User.create({
+            name,
+            cpf,
+            phoneNumber,
+            email,
+            password
+        });
+        
+        return user;
+    }
+
+    async getAllUsers() {
+        return await User.findAll();
+    }
+}
