adf8a43f19b012b80baacd18aadccf9fcd843dac
Antonio Barros <antonio.barros.alcantara.neto@ccc.ufcg.edu.br>
2025-12-01 03:12:08 -0300

    feat: Adiciona crud da entidade usuário

--- backend/src/repository/UserRepository.ts
+++ backend/src/repository/UserRepository.ts
@@ -1,1 +1,36 @@
-import { User } from "../models/User";
+import { where } from "sequelize";
+import { User, UserCreationAttributes } from "../models/User";
+
+export class UserRepository {
+
+  async createUser(data: UserCreationAttributes) {
+    const user = await User.create(data);
+
+    return user;
+  }
+
+  async getAllUsers() {
+    return await User.findAll();
+  }
+
+  async getUserById(id: number){
+    return await User.findByPk(id);
+  }
+
+  async updateUser(id:number, data: Partial<UserCreationAttributes>) {
+    const [affectedCount] = await User.update(data ,{
+      where: { id }
+    });
+
+    return affectedCount > 0;
+  }
+
+  async deleteUser(id:number) {
+    const deletedCount = await User.destroy({
+      where: { id }
+    });
+
+    return deletedCount > 0;
+  }
+
+}
